671bff717257bb21f31e3c8639c92647c37f86b8
liiora <206033431+liiora@users.noreply.github.com>
2025-04-07 08:51:38 +0200

    switch to pfui instead of shipping own libraries

--- LazyRes.lua
+++ LazyRes.lua
@@ -1,8 +1,6 @@
-local HealComm = nil;
-
 local resSpell = "Redemption";
 
-local _,playerClass = UnitClass("player");
+local _, playerClass = UnitClass("player");
 
 if playerClass == "PRIEST" then
     resSpell = "Resurrection";
@@ -10,30 +8,29 @@ elseif playerClass == "SHAMAN" then
     resSpell = "Ancestral Spirit";
 end
 
--- local classcolors = { DRUID="FF7D0A", HUNTER="ABD473", MAGE="69CCF0", PALADIN="F58CBA", PRIEST="FFFFFF", ROGUE="FFF569", SHAMAN="F58CBA", WARLOCK="9482C9", WARRIOR="C79C6E" }
+local classcolors = { DRUID="FF7D0A", HUNTER="ABD473", MAGE="69CCF0", PALADIN="F58CBA", PRIEST="FFFFFF", ROGUE="FFF569", SHAMAN="F58CBA", WARLOCK="9482C9", WARRIOR="C79C6E" }
 
 function LazyRes()
+    if not pfUI.api or not pfUI.api.libpredict then return end
 
-    if HealComm == nil then HealComm = AceLibrary("HealComm-1.0") end
-    
     CastSpellByName(resSpell);
-    
+
     local classOrder = {"SHAMAN", "PALADIN", "PRIEST", "DRUID", "MAGE", "HUNTER", "WARLOCK", "WARRIOR", "ROGUE"};
-    
-    for c=1,table.getn(classOrder) do
-        for r=1,GetNumRaidMembers() do
+
+    for c=1, table.getn(classOrder) do
+        for r=1, GetNumRaidMembers() do
             local raidId = "raid"..r;
-            
+
             local _, raidClass = UnitClass(raidId);
-    
+
             if UnitIsDead(raidId)
             and not UnitIsGhost(raidId)
-            and not HealComm:UnitisResurrecting(UnitName(raidId))
+            and not pfUI.api.libpredict:UnitHasIncomingResurrection(raidId)
             and raidClass == classOrder[c] then
                 if SpellIsTargeting() and SpellCanTargetUnit(raidId) then
                     SpellTargetUnit(raidId);
-                    -- DEFAULT_CHAT_FRAME:AddMessage("Resurrecting \124cFF"..classcolors[classOrder[c]].."\124Hitem:19:0:0:0:0:0 :0: \124h"..UnitName(raidId).."\124h\124r");
-                end                
+                    DEFAULT_CHAT_FRAME:AddMessage("Resurrecting \124cFF"..classcolors[classOrder[c]].."\124Hitem:19:0:0:0:0:0 :0: \124h"..UnitName(raidId).."\124h\124r");
+                end
             end
         end
     end
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# LazyRes
+# LazyRes for pfUI
 
 An addon for the WoW Vanilla client (1.12.1) that makes resurrecting the raid after a wipe easy.
 
-Type /lazyres (or add that command to a macro) to quickly res someone near you. Classes that can also res will be ressed before dps classes with mana, followed by dps classes without mana. The class resurrection order is Shaman/Paladin > Priest > Druid > Mage > Hunter > Warlock > Warrior > Rogue.
+Type /lazyres (or add that command to a macro) to quickly res someone near you. Classes that can also res will be ressed before dps classes with mana, followed by dps classes without mana. The class resurrection order is Shaman > Paladin > Priest > Druid > Mage > Hunter > Warlock > Warrior > Rogue.
 
 Players that are already being ressed will be skipped over. This uses the same functionality that underlies LunaUnitFrame's res indicators.
 
