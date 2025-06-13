542d8fc2db78608dd2a6c6e3012f2560028eb46f
liiora <206033431+liiora@users.noreply.github.com>
2025-06-13 07:57:22 +0200

    add group ressing

--- pfUI-lazyres.lua
+++ pfUI-lazyres.lua
@@ -1,5 +1,4 @@
-pfUI:RegisterModule("lazyres", function ()
-
+pfUI:RegisterModule("lazyres", function()
     local msg_pref = GetAddOnMetadata("pfui-lazyres", "Title") .. "|r: "
     local config
 
@@ -10,8 +9,7 @@ pfUI:RegisterModule("lazyres", function ()
         config = C.lazyres
     end
 
-
-    local classOrder = {"SHAMAN", "PALADIN", "PRIEST", "DRUID", "MAGE", "HUNTER", "WARLOCK", "WARRIOR", "ROGUE"};
+    local classOrder = {"SHAMAN", "PALADIN", "PRIEST", "DRUID", "MAGE", "HUNTER", "WARLOCK", "WARRIOR", "ROGUE"}
     local classcolors = { DRUID="FF7D0A", HUNTER="ABD473", MAGE="69CCF0", PALADIN="F58CBA", PRIEST="FFFFFF", ROGUE="FFF569", SHAMAN="2459FF", WARLOCK="9482C9", WARRIOR="C79C6E" }
     local _, playerClass = UnitClass("player")
     local resSpell
@@ -19,42 +17,46 @@ pfUI:RegisterModule("lazyres", function ()
         resSpell = "Resurrection"
     elseif playerClass == "SHAMAN" then
         resSpell = "Ancestral Spirit"
-    else
+    elseif playerClass == "PALADIN" then
         resSpell = "Redemption"
+    else
+        DEFAULT_CHAT_FRAME:AddMessage(msg_pref .. "No resurrection spell found. Exiting addon ...")
+        return
     end
-    local function SlashHandler(msg)
-        local isRessing = false
-        CastSpellByName(resSpell);
-        for c=1, table.getn(classOrder) do
-            for r=1, GetNumRaidMembers() do
-                local raidId = "raid"..r;
 
-                local _, raidClass = UnitClass(raidId);
+    local function Log(msg)
+        if config.notification == "1" then
+            DEFAULT_CHAT_FRAME:AddMessage(msg_pref .. msg)
+        end
+    end
 
-                if UnitIsDead(raidId)
-                    and raidClass == classOrder[c]
-                    and not UnitIsGhost(raidId)
-                    and not pfUI.api.libpredict:UnitHasIncomingResurrection(raidId)
-                    and SpellIsTargeting()
-                    and SpellCanTargetUnit(raidId) then
+    local function IsInParty() return GetNumRaidMembers() == 0 and GetNumPartyMembers() > 0 end
 
-                        isRessing = true
-                        SpellTargetUnit(raidId)
+    _G.SLASH_LAZYRES1 = "/pflr"
+    function _G.SlashCmdList.LAZYRES()
+        CastSpellByName(resSpell)
 
-                        if config.notification == "1" then
-                            DEFAULT_CHAT_FRAME:AddMessage(msg_pref .. "resurrecting \124cFF"..classcolors[classOrder[c]].."\124Hitem:19:0:0:0:0:0 :0: \124h"..UnitName(raidId).."\124h\124r");
-                        end
+        local numMembers = IsInParty() and GetNumPartyMembers() or GetNumRaidMembers()
+        local unitPrefix = IsInParty() and "party" or "raid"
+        for cId=1, table.getn(classOrder) do
+            for mId=1, numMembers do
+                local unitId = unitPrefix..mId
+                local _, unitClass = UnitClass(unitId)
+                if unitClass == classOrder[cId]
+                and UnitIsDead(unitId)
+                and not UnitIsGhost(unitId)
+                and not pfUI.api.libpredict:UnitHasIncomingResurrection(unitId)
+                and SpellIsTargeting()
+                and SpellCanTargetUnit(unitId) then
+                    SpellTargetUnit(unitId)
+                    Log("resurrecting \124cFF"..classcolors[classOrder[cId]].."\124Hitem:19:0:0:0:0:0 :0: \124h"..UnitName(unitId).."\124h\124r")
+                    return
                 end
             end
         end
 
-        if not isRessing and config.notification == "1" then
-            DEFAULT_CHAT_FRAME:AddMessage(msg_pref .. " select target to ress");
-        end
+        Log("select target to resurrecting")
     end
-
-    _G.SLASH_LAZYRES1 = "/pflr"
-    _G.SlashCmdList.LAZYRES = SlashHandler
 
     pfUI.lazyres:SetScript("OnEvent", pfUI.lazyres.Load)
 end)
